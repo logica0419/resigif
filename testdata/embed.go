07885196acefa277eaf57befa8e52b50f4e2e283
logica0419 <84759675+logica0419@users.noreply.github.com>
2024-02-03 07:29:43 +0900

    add testdata

---
+++ testdata/embed.go
@@ -0,0 +1,23 @@
+// Package testdata provides test data
+package testdata
+
+import "embed"
+
+// FS migration file system
+//
+//go:embed *.gif
+var FS embed.FS
+
+// GIF画像 出典
+
+// frog.gif https://sozai-good.com/illust/gifanimation/29065
+
+// miku.gif https://piapro.jp/t/FB3J
+//	marucaさんの作品
+
+// mushroom.gif http://www.ugokue.com
+//	【動け!!動く絵】様より
+
+// new_year.gif https://freesozaixtrain.web.fc2.com/freesozai-nenga-train2.html
+
+// tooth.gif https://patirabi.com/2021/10/10/061gif/
